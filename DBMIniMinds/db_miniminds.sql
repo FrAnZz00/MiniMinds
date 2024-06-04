5b0572b9bb1fd884885e2cd4de7cf4861e3dcf8b
h0om4n71030@gmail.com <h0om4n71030@email.com>
2024-06-04 08:07:40 +0800

    added databaseM

--- DBMIniMinds/db_miniminds.sql
+++ DBMIniMinds/db_miniminds.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2024 at 09:24 AM
+-- Generation Time: Jun 04, 2024 at 02:01 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,6 +40,19 @@ CREATE TABLE `failed_jobs` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_kelas` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -57,7 +70,14 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2024_05_23_070007_create_progress_table', 1),
+(6, '2024_05_25_111104_tb_soal', 1),
+(7, '2024_05_25_111112_tb_progress', 1),
+(8, '2024_05_25_111120_tb_nilai', 1),
+(9, '2024_05_25_111129_tb_level', 1),
+(10, '2024_05_25_111138_tb_kategori', 1),
+(11, '2024_05_25_111334_tb_kelas', 1);
 
 -- --------------------------------------------------------
 
@@ -93,18 +113,30 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `progress`
+--
+
+CREATE TABLE `progress` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_kategori`
 --
 
 CREATE TABLE `tb_kategori` (
-  `ID_kategori` int(11) NOT NULL,
-  `angka` varchar(50) NOT NULL,
-  `huruf` varchar(50) NOT NULL,
-  `warna` varchar(50) NOT NULL,
-  `bentuk` varchar(50) NOT NULL,
-  `mewarnai` varchar(50) NOT NULL,
-  `menggambar` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id_kategori` bigint(20) UNSIGNED NOT NULL,
+  `angka` varchar(255) NOT NULL,
+  `huruf` varchar(255) NOT NULL,
+  `warna` varchar(255) NOT NULL,
+  `bentuk` varchar(255) NOT NULL,
+  `mewarnai` varchar(255) NOT NULL,
+  `menggambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -113,12 +145,12 @@ CREATE TABLE `tb_kategori` (
 --
 
 CREATE TABLE `tb_level` (
-  `ID_level` int(11) NOT NULL,
-  `ID_kategori` int(11) DEFAULT NULL,
-  `level_1` varchar(10) DEFAULT NULL,
-  `level_2` varchar(10) DEFAULT NULL,
-  `level_3` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID_level` bigint(20) UNSIGNED NOT NULL,
+  `ID_kategori` int(11) NOT NULL,
+  `level_1` varchar(255) NOT NULL,
+  `level_2` varchar(255) NOT NULL,
+  `level_3` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -127,11 +159,11 @@ CREATE TABLE `tb_level` (
 --
 
 CREATE TABLE `tb_nilai` (
-  `ID_nilai` int(11) NOT NULL,
-  `ID_level` int(11) DEFAULT NULL,
-  `ID_users` int(11) DEFAULT NULL,
-  `nilai` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID_nilai` bigint(20) UNSIGNED NOT NULL,
+  `ID_level` int(11) NOT NULL,
+  `ID_users` int(11) NOT NULL,
+  `nilai` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -140,12 +172,12 @@ CREATE TABLE `tb_nilai` (
 --
 
 CREATE TABLE `tb_progress` (
-  `ID_progress` int(11) NOT NULL,
-  `ID_users` int(11) DEFAULT NULL,
-  `ID_kategori` int(11) DEFAULT NULL,
-  `ID_level` int(11) DEFAULT NULL,
-  `progress` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID_progress` bigint(20) UNSIGNED NOT NULL,
+  `ID_users` int(11) NOT NULL,
+  `ID_kategori` int(11) NOT NULL,
+  `ID_level` int(11) NOT NULL,
+  `progress` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -154,9 +186,9 @@ CREATE TABLE `tb_progress` (
 --
 
 CREATE TABLE `tb_soal` (
-  `ID_soal` int(11) NOT NULL,
-  `ID_level` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID_soal` bigint(20) UNSIGNED NOT NULL,
+  `ID_level` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -165,24 +197,27 @@ CREATE TABLE `tb_soal` (
 --
 
 CREATE TABLE `users` (
-  `ID_users` int(11) NOT NULL,
-  `nama` varchar(50) DEFAULT NULL,
-  `password` text DEFAULT NULL,
-  `role` enum('Guru','Murid') DEFAULT NULL,
-  `updated_at` date NOT NULL DEFAULT curdate(),
-  `created_at` date NOT NULL DEFAULT curdate()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('murid','guru') NOT NULL,
+  `kelas` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`ID_users`, `nama`, `password`, `role`, `updated_at`, `created_at`) VALUES
-(1, 'Ibu Kiki', '$2y$10$T3Nk6XfU1.TX1qUB88DSmOlHrsMd3aFFXnB7/qj6gnIFaVfBNrYcW', NULL, '2024-05-12', '2024-05-12'),
-(2, 'Aurel', NULL, NULL, '2024-05-12', '2024-05-12'),
-(17, 'Ibu Hanni', '$2y$10$wDjWuAZAFFCNpUCPBKty7eiDofwyo1ZH2QVXgctwH4D23wfPK59Ua', NULL, '2024-05-14', '2024-05-14'),
-(18, 'Aku Aurel', '$2y$10$V7.pVfkZtOJb8EjRyt26WuKH5onyRsrZYcu5pdDUmWsnD.GCekvhK', 'Guru', '2024-05-21', '2024-05-21'),
-(19, 'ini saya', '$2y$10$v61QXXH27J5KVKIvfnT8ROya2A6r3oDdOAYx506f0xUkWHT73a99a', 'Murid', '2024-05-21', '2024-05-21');
+INSERT INTO `users` (`id`, `username`, `fullname`, `password`, `role`, `kelas`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'kiki', 'Ibu Kiki', '$2y$10$iyk02TBNcj0GZZ51cO1FLOPnbEB9ZAzmq4MFmtTKoLU1YVUrkirpi', 'guru', 'A1', NULL, NULL, NULL),
+(2, 'aurel', 'Aurel', '$2y$10$2xBrd7jjUO1mi1FUczRpSepRR/KpXIZZh5AP9lCBa23NlBfcOkZAC', 'murid', 'A1', NULL, NULL, NULL),
+(3, 'Aurellia', 'Aurellia Az Zahra', '$2y$10$5.UVTIhRusMriuCfEKz90ebShSXqlSaE/Bw4b2TGgLV1qdE9VtIEq', 'murid', 'B1', NULL, '2024-05-26 14:59:55', '2024-05-26 14:59:55'),
+(4, 'Yazid', 'Yazid Ikram M', '$2y$10$NXkHvHY9zx6RhG55LHz/xOVySjDc74nGBk1t0gMDeKg3iSJso8NEW', 'guru', 'A1', NULL, '2024-05-26 15:04:58', '2024-05-26 15:04:58'),
+(5, 'Adit', 'Adit Rengga', '$2y$10$hwt3BizqBgqPEMHctwftoOd537H4vjwESFyikGQqiGMkCSz4dQtx2', 'murid', 'B1', NULL, '2024-05-27 18:30:52', '2024-05-27 18:30:52');
 
 --
 -- Indexes for dumped tables
@@ -194,6 +229,12 @@ INSERT INTO `users` (`ID_users`, `nama`, `password`, `role`, `updated_at`, `crea
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `migrations`
@@ -216,46 +257,46 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Indexes for table `progress`
+--
+ALTER TABLE `progress`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tb_kategori`
 --
 ALTER TABLE `tb_kategori`
-  ADD PRIMARY KEY (`ID_kategori`);
+  ADD PRIMARY KEY (`id_kategori`);
 
 --
 -- Indexes for table `tb_level`
 --
 ALTER TABLE `tb_level`
-  ADD PRIMARY KEY (`ID_level`),
-  ADD KEY `ID_kategori` (`ID_kategori`);
+  ADD PRIMARY KEY (`ID_level`);
 
 --
 -- Indexes for table `tb_nilai`
 --
 ALTER TABLE `tb_nilai`
-  ADD PRIMARY KEY (`ID_nilai`),
-  ADD KEY `ID_level` (`ID_level`);
+  ADD PRIMARY KEY (`ID_nilai`);
 
 --
 -- Indexes for table `tb_progress`
 --
 ALTER TABLE `tb_progress`
-  ADD PRIMARY KEY (`ID_progress`),
-  ADD KEY `ID_users` (`ID_users`),
-  ADD KEY `ID_kategori` (`ID_kategori`),
-  ADD KEY `ID_level` (`ID_level`);
+  ADD PRIMARY KEY (`ID_progress`);
 
 --
 -- Indexes for table `tb_soal`
 --
 ALTER TABLE `tb_soal`
-  ADD PRIMARY KEY (`ID_soal`),
-  ADD KEY `ID_level` (`ID_level`);
+  ADD PRIMARY KEY (`ID_soal`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`ID_users`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -268,10 +309,16 @@ ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -280,70 +327,46 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `progress`
+--
+ALTER TABLE `progress`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `tb_kategori`
 --
 ALTER TABLE `tb_kategori`
-  MODIFY `ID_kategori` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kategori` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_level`
 --
 ALTER TABLE `tb_level`
-  MODIFY `ID_level` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_level` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_nilai`
 --
 ALTER TABLE `tb_nilai`
-  MODIFY `ID_nilai` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_nilai` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_progress`
 --
 ALTER TABLE `tb_progress`
-  MODIFY `ID_progress` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_progress` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_soal`
 --
 ALTER TABLE `tb_soal`
-  MODIFY `ID_soal` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_soal` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `tb_level`
---
-ALTER TABLE `tb_level`
-  ADD CONSTRAINT `tb_level_ibfk_1` FOREIGN KEY (`ID_kategori`) REFERENCES `tb_kategori` (`ID_kategori`);
-
---
--- Constraints for table `tb_nilai`
---
-ALTER TABLE `tb_nilai`
-  ADD CONSTRAINT `tb_nilai_ibfk_1` FOREIGN KEY (`ID_level`) REFERENCES `tb_level` (`ID_level`);
-
---
--- Constraints for table `tb_progress`
---
-ALTER TABLE `tb_progress`
-  ADD CONSTRAINT `tb_progress_ibfk_1` FOREIGN KEY (`ID_users`) REFERENCES `users` (`ID_users`),
-  ADD CONSTRAINT `tb_progress_ibfk_2` FOREIGN KEY (`ID_kategori`) REFERENCES `tb_kategori` (`ID_kategori`),
-  ADD CONSTRAINT `tb_progress_ibfk_3` FOREIGN KEY (`ID_level`) REFERENCES `tb_level` (`ID_level`);
-
---
--- Constraints for table `tb_soal`
---
-ALTER TABLE `tb_soal`
-  ADD CONSTRAINT `tb_soal_ibfk_1` FOREIGN KEY (`ID_level`) REFERENCES `tb_level` (`ID_level`);
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
